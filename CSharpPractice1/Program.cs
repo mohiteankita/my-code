f0ed3953b2c12851444b7fda2cc9db16a424ef35
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-02 14:25:28 +0530

    Add files via upload

---
+++ CSharpPractice1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CSharpPractice1
+{
+    class Program
+    {
+       [AttributeUsage(AttributeTargets.Class)]
+        class NotInUse : System.Attribute
+        {
+            public bool IsUsed
+            {
+                get;
+                set;
+            }
+        }
+        [NotInUse(IsUsed = false)]
+        class Demo
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Type t = typeof(Demo);
+            foreach(Attribute a in t.GetCustomAttributes(false))
+            {
+                NotInUse na = (NotInUse)a;
+                if(na.IsUsed == true)
+                {
+                    Console.WriteLine("Class cannot be used");
+                }
+                else
+                {
+                    Console.WriteLine("Class is used");
+                }
+            }
+        }
+    }
+}
