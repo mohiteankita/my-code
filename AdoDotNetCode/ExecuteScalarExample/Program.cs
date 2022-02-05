83ccbfeebe8fbb5b9b0752e9c7d92660792a0d87
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-05 13:21:04 +0530

    Add files via upload

---
+++ AdoDotNetCode/ExecuteScalarExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ExecuteScalarExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection("Server=LAPTOP-81HIQF3R\\SQLEXPRESS;database=HRMSDB;trusted_connection=true");
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand("select sum(salary) as total_salary from employees", connection);
+                cmd.CommandType = CommandType.Text;
+
+                decimal total_salary = (decimal)cmd.ExecuteScalar();
+                Console.WriteLine("Total salary is " + total_salary);
+                connection.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
