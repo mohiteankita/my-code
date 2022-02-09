38809a01dce644a41c38bad37b33924233507e5a
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-09 12:46:18 +0530

    Add files via upload

---
+++ Assignment1/FibonacciSeries/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FibonacciSeries
+{
+    class Program
+    {
+        public void PrintFibonacciSeries()
+        {
+            int a = 0, b = 1, temp;
+            
+            for (int i=0; i<8; i++)
+            {
+                Console.Write(a + " ");
+                temp = a + b;
+                a = b;
+                b = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Program().PrintFibonacciSeries();
+        }
+    }
+}
