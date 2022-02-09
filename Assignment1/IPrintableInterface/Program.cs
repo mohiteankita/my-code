6afa326b9ad0d4a0f15482d4497124ee51a1695f
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-09 12:51:01 +0530

    Add files via upload

---
+++ Assignment1/IPrintableInterface/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace IPrintableInterface
+{
+    interface IPrintable
+    {
+        void Print();
+    }
+
+    class Employee : IPrintable
+    {
+        int empid;
+        string empname;
+
+        public Employee(int empid, string empname)
+        {
+            this.empid = empid;
+            this.empname = empname;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Employee id= " + empid + ", Employee name= " + empname);
+        }
+    }
+
+    class Student : IPrintable
+    {
+        int studid;
+        string studname;
+
+        public Student(int studid, string studname)
+        {
+            this.studid = studid;
+            this.studname = studname;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Student id= " + studid + ", Student name= " + studname);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter employee id: ");
+            int empid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee name: ");
+            string empname = Console.ReadLine();
+            
+            IPrintable obj = new Employee(empid, empname);
+            obj.Print();
+            Console.WriteLine("_______________________________________________________");
+
+            Console.WriteLine("Enter student id: ");
+            int studid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter student name: ");
+            string studname = Console.ReadLine();
+            IPrintable obj1 = new Student(studid, studname);
+            obj1.Print();
+        }
+    }
+}
