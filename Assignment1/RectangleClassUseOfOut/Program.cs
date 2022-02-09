c7d5d73d9e80ed963d8dca7452dda6d569cd2513
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-09 12:53:15 +0530

    Add files via upload

---
+++ Assignment1/RectangleClassUseOfOut/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RectangleClassUseOfOut
+{
+    class Rectangle
+    {
+        public void Calculate(float length, float breadth, out double area, out double perimeter)
+        {
+            area = length * breadth;
+            perimeter = 2 * (length + breadth);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, perimeter;
+
+            Console.WriteLine("Enter Length of Rectangle: ");
+            float length = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Breadth of Rectangle: ");
+            float breadth = float.Parse(Console.ReadLine());
+
+            Rectangle r = new Rectangle();
+            r.Calculate(length, breadth, out area, out perimeter);
+
+            Console.WriteLine("Area of Rectangle= " + area + "\nPerimeter of Rectangle= " + perimeter);
+        }
+    }
+}
