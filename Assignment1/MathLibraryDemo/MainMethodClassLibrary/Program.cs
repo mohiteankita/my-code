a7e8cf75f6e5df81c8b14a40bdb21ef31759f135
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-10 10:52:16 +0530

    Add files via upload

---
+++ Assignment1/MathLibraryDemo/MainMethodClassLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MathLibraryDemo;
+using System.Reflection;
+
+namespace MainMethodClassLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number to find square and cube: ");
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            //By Reflection
+            Type t = typeof(MathLibraryDemo.MathLibrary);
+            MethodInfo[] methodinfo = t.GetMethods();
+            foreach(MethodInfo method in methodinfo)
+            {
+                object[] parameter = new object[1];
+                parameter[0] = no;
+
+                if(method.Name=="Square" || method.Name=="Cube")
+                {
+                    object o = Activator.CreateInstance(t);
+                    object result = method.Invoke(o, parameter);
+                    Console.WriteLine(method.Name + " of " + no + "= " + result);
+                }
+            }
+        }
+    }
+}
