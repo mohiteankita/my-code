a9773fa1ff79214f119baca69e34ebee48290004
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-10 10:53:59 +0530

    Add files via upload

---
+++ Assignment1/StoreDataIntoFile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace StoreDataIntoFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream filestream = new FileStream("MyData.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(filestream);
+
+            Console.WriteLine("Enter data to write into file");
+            string fileinfo = Console.ReadLine();
+            writer.WriteLine(fileinfo);
+            writer.Flush();
+
+            Console.WriteLine("Data Stored into File Successfully");
+        }
+    }
+}
