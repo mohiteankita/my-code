a1493edd500c74ecc0b925757ee0c541a87b3845
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-10 10:49:47 +0530

    Add files via upload

---
+++ Assignment1/MathLibraryDemo/MathLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathLibraryDemo
+{
+    public class MathLibrary
+    {
+        public int Square(int no)
+        {
+            return no * no;
+        }
+        public long Cube(int no)
+        {
+            return no * no * no;
+        }
+    }
+}
