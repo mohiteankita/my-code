fd7c5d582a663bdd0a6929d9473f0c3672ab8ece
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-12 09:59:15 +0530

    Add files via upload

---
+++ Assignment1/CheckCapitalSmallLetterDigitSpeciChar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CheckCapitalSmallLetterDigitSpeciChar
+{
+    class Program
+    {
+        public void Validation(char ch)
+        {
+            if(ch>=65 && ch<=90)
+            {
+                Console.WriteLine(ch + " is a capital letter");
+            }
+            else if(ch>=97 && ch<=122)
+            {
+                Console.WriteLine(ch + " is a small letter");
+            }
+            else if(ch>=48 && ch<=57)
+            {
+                Console.WriteLine(ch + " is a digit");
+            }
+            else
+            {
+                Console.WriteLine(ch + " is a special character");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Character: ");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            Program p = new Program();
+            p.Validation(ch);
+        }
+    }
+}
