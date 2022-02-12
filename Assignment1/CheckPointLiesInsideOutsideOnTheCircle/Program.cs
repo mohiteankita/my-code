cc7430246ab709a4606d064082de142204fc34d2
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-12 12:32:29 +0530

    Add files via upload

---
+++ Assignment1/CheckPointLiesInsideOutsideOnTheCircle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CheckPointLiesInsideOutsideOnTheCircle
+{
+    class Program
+    {
+        public void Circle()
+        {
+            Console.WriteLine("Enter the radius of circle: ");
+            float radius = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the (x, y) co-ordinates of the point for checking it's position: ");
+            float x = float.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+
+            if(radius > Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)))
+            {
+                Console.WriteLine("The point lie inside the circle");
+            }
+            else if(radius == Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2)))
+            {
+                Console.WriteLine("The point lie on the circle");
+            }
+            else
+            {
+                Console.WriteLine("The point lie outside the circle");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Circle();
+        }
+    }
+}
