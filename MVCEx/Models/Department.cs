032359586effc76b3379673e1229f8c529d14ad6
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-16 23:02:36 +0530

    Add files via upload

---
+++ MVCEx/Models/Department.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCExample.Models
+{
+    public class Department
+    {
+        public int deptid { get; set; }
+
+        public string deptname { get; set; }
+
+        public string location { get; set; }
+
+        public string country { get; set; }
+    }
+}
---
+++ MVCEx/Models/DeptBal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCExample.Models
+{
+    public class DeptBal
+    {
+        public List<Department> GetDeptList()
+        {
+            List<Department> dept_list = new List<Department>();
+
+            dept_list.Add(new Department() { deptid = 111, deptname = "Sales", location = "Pune", country = "India" });
+            dept_list.Add(new Department() { deptid = 112, deptname = "Admin", location = "Nagpur", country = "India" });
+            dept_list.Add(new Department() { deptid = 113, deptname = "Accounting", location = "Mumbai", country = "India" });
+            dept_list.Add(new Department() { deptid = 114, deptname = "Purchase", location = "Mumbai", country = "India" });
+
+            return dept_list;
+        }
+    }
+}
