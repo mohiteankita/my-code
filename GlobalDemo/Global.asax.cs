831f8e25264303965b8c59b7ba7149274b21eef1
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-16 22:20:02 +0530

    Add files via upload

---
+++ GlobalDemo/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace GlobalDemo
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Console.WriteLine("Application Started");
+            Application["appcounter"] = 500;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            Console.WriteLine("Session Started");
+            Session["sesscounter"] = 600;
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+            Console.WriteLine("Session Ended");
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            Console.WriteLine("Application Ended");
+        }
+    }
+}
---
+++ GlobalDemo/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GlobalDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int appcode = Convert.ToInt32((Application["appcounter"]).ToString());
+            appcode++;
+            Application["appcounter"] = appcode;
+
+            int sesscode = Convert.ToInt32((Session["sesscounter"]).ToString());
+            sesscode++;
+           
+            Response.Write("Application Code " + appcode);
+            Response.Write("Session Code " + sesscode);
+        }
+    }
+}
