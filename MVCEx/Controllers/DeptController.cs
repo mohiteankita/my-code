307ac45f541a803624357f1762ee0699c6d819a6
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-16 23:00:30 +0530

    Add files via upload

---
+++ MVCEx/Controllers/DeptController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCExample.Models;
+
+namespace MVCExample.Controllers
+{
+    public class DeptController : Controller
+    {
+        // GET: Dept
+        public ActionResult Index()
+        {
+            DeptBal bal = new DeptBal();
+            List<Department> dept_list = bal.GetDeptList();
+            TempData["deptlist"] = dept_list;
+            return View();
+        }
+    }
+}
---
+++ MVCEx/Controllers/EmpController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET: Emp
+        [HttpGet]
+        public ActionResult Index()
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            string name = collection["TxtName"].ToString();
+            Session["name"] = name;
+            return View();
+        }
+    }
+}
