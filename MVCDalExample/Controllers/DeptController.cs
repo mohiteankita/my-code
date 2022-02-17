029652fdc82657d19683d46fb0c8bb0deb0cfa42
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-17 15:42:16 +0530

    Add files via upload

---
+++ MVCDalExample/Controllers/DeptController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDalEx1.Models;
+
+namespace MVCDalEx1.Controllers
+{
+    public class DeptController : Controller
+    {
+        DepartmentDal dal = new DepartmentDal();
+        // GET: Dept
+        public ActionResult Index()
+        {
+            List<Department> deptlist = dal.GetDeptList();
+            return View(deptlist);
+        }
+
+        [HttpGet]
+        public ActionResult Insert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Insert(FormCollection collection)
+        {
+            Department dept = new Department();
+            dept.deptid = Convert.ToInt32(collection["deptid"]);
+            dept.deptname = collection["deptname"].ToString();
+            dept.location = collection["location"].ToString();
+            dept.country = collection["country"].ToString();
+            dal.InsertDepartment(dept);
+            return View();
+        }
+
+        public ActionResult Update(int deptid)
+        {
+            Department dept = dal.GetDepartment(deptid);
+            return View(dept);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Department d)
+        {
+            dal.UpdateDepartment(d);
+            return View(d);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Department dept = dal.GetDepartment(id);
+            return View(dept);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Department dept = dal.GetDepartment(id);
+            return View(dept);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(FormCollection collection)
+        {
+            Department dept = dal.GetDepartment(Convert.ToInt32(collection["deptid"]));
+            dal.DeleteDepartment(dept);
+            return RedirectToAction("Index");
+        }
+    }
+}
