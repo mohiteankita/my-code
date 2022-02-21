9ed7ebe37fdbd01eb236a58eade090ce85a0d5a5
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-21 23:24:10 +0530

    Add files via upload

---
+++ DemoAPI/APICalling/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CallingAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44310/api/Demo/");
+            var resp = client.GetAsync("https://localhost:44310/api/Demo/");
+            var result = resp.Result;
+            if(result.IsSuccessStatusCode)
+            {
+                var res = result.Content.ReadAsAsync<string>();
+                res.Wait();
+                string restext = res.Result;
+                TempData["APIResp"] = restext;
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
