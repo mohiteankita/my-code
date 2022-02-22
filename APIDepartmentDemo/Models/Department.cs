e7105d481eb1a3fbe67ced5acec74f4210e0f064
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-22 14:38:12 +0530

    Add files via upload

---
+++ APIDepartmentDemo/Models/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DepartmentAPI.Models
+{
+    public class Department
+    {
+        [Required(ErrorMessage = "Please Enter Department ID It Cannot Be Left Blank")]
+        public int deptid { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Department Name")]
+        public string deptname { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Location")]
+        public string location { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Country")]
+        public string country { get; set; }
+    }
+}
