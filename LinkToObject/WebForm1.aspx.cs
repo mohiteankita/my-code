ec858039fbc4a2a061676d3803e6e7d88fe879bf
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-24 11:24:28 +0530

    Add files via upload

---
+++ LinkToObject/WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using MVCExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinkToObject
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DeptBal bal = new DeptBal();
+            List<Department> dlist = bal.GetDeptList();
+            //var res = from data in dlist
+            //          where data.deptid < 113
+            //          select new { data.deptid, data.deptname };
+
+            //var res = from data in dlist
+            //          where data.deptid < 113
+            //          select data;
+
+            var res = from data in dlist
+                      where data.deptname.Length > 6
+                      select data;
+
+            GridView1.DataSource = res;
+            GridView1.DataBind();
+        }
+    }
+}
