b90268d3045980f57656a9fca0fcfd855db3eada
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-28 10:33:07 +0530

    Add files via upload

---
+++ AutofacDemo/AutofacDemo/AutofacDemo1/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofacDemo1
+{
+    public class EmailService : IMailService
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Email Service");
+        }
+    }
+}
---
+++ AutofacDemo/AutofacDemo/AutofacDemo1/NotificationSender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofacDemo1
+{
+    class NotificationSender
+    {
+        public IMobileService _mobileService = null;
+        public IMailService _mailService = null;
+
+        //Injection Through Constructor
+        public NotificationSender(IMobileService tmpService)
+        {
+            _mobileService = tmpService;
+        }
+
+        //Injection Through Property
+        public IMailService SetMailService
+        {
+            set
+            {
+                _mailService = value;
+            }
+        }
+
+        public void SendNotification()
+        {
+            _mobileService.Execute();
+            _mailService.Execute();
+        }
+    }
+}
