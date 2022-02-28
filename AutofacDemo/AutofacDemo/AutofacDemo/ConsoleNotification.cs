f20db40eb8447d8c1922a427ba2ba4835792e854
mohiteankita <89238784+mohiteankita@users.noreply.github.com>
2022-02-28 10:31:53 +0530

    Add files via upload

---
+++ AutofacDemo/AutofacDemo/AutofacDemo/ConsoleNotification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutofacDemo
+{
+    internal class ConsoleNotification : INotificationService
+    {
+        public void NotifyUsernameChanged(User user)
+        {
+            Console.WriteLine($"Username has been changed to: {user.Username}");
+        }
+    }
+}
